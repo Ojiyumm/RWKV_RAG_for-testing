b2fd0147dcc2dd220efd4e353ca7055276ff99b1
Ojiyumm <125381866+Ojiyumm@users.noreply.github.com>
2024-07-30 22:27:52 +0800

    Delete ragq directory

--- ragq/src/test/test_tuning_client.py
+++
@@ -1,8 +0,0 @@
-from src.clients.tuning_client import RWKVPEFTClient
-
-
-client = RWKVPEFTClient('tcp://localhost:7789')
-client.state_tuning_train(load_model='/home/rwkv/Peter/model/base/RWKV-x060-World-1B6-v2.1-20240328-ctx4096.pth',
-                          proj_dir='/home/rwkv/LongHua/project_log',
-                          data_file='/home/rwkv/Peter/Data/Telechat5/top_2k',
-                          micro_bsz=2)
